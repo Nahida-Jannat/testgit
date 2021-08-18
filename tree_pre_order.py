93bb9fefc982ed74094c68a8d0329d116ec12447
Nahida-Jannat <86676326+Nahida-Jannat@users.noreply.github.com>
2021-08-18 23:19:49 +0600

    Tree Implementation Done

---
+++ tree_pre_order.py
@@ -0,0 +1,65 @@
+# Tree implementation post-order
+
+"""
+            6
+         /   \
+         5     9
+      /  \      \
+      3   11      10
+         / \    /  \
+         7   2  4    8
+"""
+
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.left = None
+      self.right = None
+
+   def add_left(self, node):
+      self.left = node
+
+   def add_right(self, node):
+      self.right = node
+
+
+def create_tree():
+   six = Node(6)
+   five = Node(5)
+   nine = Node(9)
+   six.add_left(five)
+   six.add_right(nine)
+   
+   three = Node(3)
+   eleven = Node(11)
+   five.add_left(three)
+   five.add_right(eleven)
+
+   seven = Node(7)
+   two = Node(2)
+   eleven.add_left(seven)
+   eleven.add_right(two)
+
+   ten = Node(10)
+   nine.add_right(ten)
+
+   four = Node(4)
+   eight = Node(8)
+   ten.add_left(four)
+   ten.add_right(eight)
+   # return root node
+   return six
+
+
+def pre_order(node):
+   print(node.data, end=' ')
+   if node.left:
+      pre_order(node.left)
+   if node.right:
+      pre_order(node.right)
+
+if __name__ == "__main__":
+   tree = create_tree()
+   print(tree.data)
+   print('\n')
+   pre_order(tree)
